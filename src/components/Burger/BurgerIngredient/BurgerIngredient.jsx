865854b871af98a949a7c4765a61def9191eb574
rudis <rudislpp@gmail.com>
2018-12-30 09:56:41 -0200

    added modal

--- src/components/Burger/BurgerIngredient/BurgerIngredient.jsx
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.jsx
@@ -1,43 +1,43 @@
 import React, { Component } from 'react';
-import classes from './BurgerIngredient.css';
 import propTypes from 'prop-types';
+import classes from './BurgerIngredient.css';
 
 class BurgerIngredient extends Component {
-  render () {
+  render() {
     let ingredient = null;
 
     switch (this.props.type) {
       case 'bread-bottom':
-        ingredient = <div className={classes.BreadBottom}></div>
+        ingredient = <div className={classes.BreadBottom} />;
         break;
       case 'bread-top':
         ingredient = (
           <div className={classes.BreadTop}>
-            <div className={classes.Seeds1}></div>
-            <div className={classes.Seeds2}></div>
+            <div className={classes.Seeds1} />
+            <div className={classes.Seeds2} />
           </div>
-        )
+        );
         break;
       case 'meat':
-        ingredient = <div className={classes.Meat}></div>
+        ingredient = <div className={classes.Meat} />;
         break;
       case 'cheese':
-        ingredient = <div className={classes.Cheese}></div>
+        ingredient = <div className={classes.Cheese} />;
         break;
       case 'bacon':
-        ingredient = <div className={classes.Bacon}></div>
+        ingredient = <div className={classes.Bacon} />;
         break;
       case 'salad':
-        ingredient = <div className={classes.Salad}></div>
+        ingredient = <div className={classes.Salad} />;
         break;
       default:
         ingredient = null;
         break;
     }
-  
+
     return ingredient;
   }
-} 
+}
 
 BurgerIngredient.propTypes = {
   type: propTypes.string.isRequired,
--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -14,6 +16,7 @@ class BurgerBuilder extends Component {
   //   super(props);
   //   this.state = {...};
   // }
+  
   state = {
     ingredients: {
       salad: 0,
@@ -21,8 +24,28 @@ class BurgerBuilder extends Component {
       cheese: 0,
       meat: 0,
     },
-    totalPrice: 4
+    totalPrice: 4,
+    purchasable: false,
+    purchasing: false,
   };
+
+  purchaseHandler = () => {
+    this.setState({purchasing: true});
+  }
+
+  purchaseCancelHandler = () => {
+    this.setState({purchasing: false});
+  }
+
+  updatePurchaseState (ingredients) {
+    const sum = Object.keys(ingredients)
+      .map(igKey => {
+        return ingredients[igKey];
+      }).reduce((sum, el) => {
+        return sum + el;
+      }, 0);
+    this.setState(({purchasable: sum > 0}))
+  }
 
   addIngredientHandler = (type) => {
     const oldCount = this.state.ingredients[type];
@@ -35,7 +58,8 @@ class BurgerBuilder extends Component {
     const newPrice = oldPrice + priceAddition;
 
     updateIngredients[type] = updatedCount;
-    this.setState({totalPrice: newPrice, ingredients: updateIngredients})
+    this.setState({totalPrice: newPrice, ingredients: updateIngredients});
+    this.updatePurchaseState(updateIngredients);
   }
   
   removeIngredientHandler = (type) => {
@@ -49,10 +73,11 @@ class BurgerBuilder extends Component {
     const updateIngredients = {
       ...this.state.ingredients,
     }
-    const newPrice = oldPrice + priceDeduction;
+    const newPrice = oldPrice - priceDeduction;
 
     updateIngredients[type] = updatedCount;
     this.setState({totalPrice: newPrice, ingredients: updateIngredients});
+    this.updatePurchaseState(updateIngredients);
   }
   render () {
     const disabledInfo = {
@@ -65,11 +90,17 @@ class BurgerBuilder extends Component {
     
     return (
       <Aux>
+        <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+          <OrderSummary ingredients={this.state.ingredients}/>
+        </Modal>
         <Burger ingredients={ this.state.ingredients }/>
         <BuildControls 
         ingredientAdded={ this.addIngredientHandler }
         ingredientRemoved={ this.removeIngredientHandler }
         disabled={disabledInfo}
+        price={this.state.totalPrice}
+        purchasable={this.state.purchasable}
+        ordered={this.purchaseHandler}
         />
       </Aux>
     );
